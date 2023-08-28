4664023ce4e364d230dfec8ba90ebb9e171f9d9f
Angel <mikeqlow1517@gmail.com>
2023-08-28 17:13:47 -0500

    Se agrega Proyecto

--- src/ConversorDeMonedas.java
+++ src/ConversorDeMonedas.java
@@ -1,10 +1,18 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class ConversorDeMonedas {
 
 	private JFrame frame;
+	private JTextField texto;
+	private JButton btn;
+	private JComboBox cmb;
+	private JLabel lbl;
 
 	/**
 	 * Launch the application.
@@ -36,6 +44,23 @@ public class ConversorDeMonedas {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		texto = new JTextField();
+		texto.setBounds(10, 10, 96, 19);
+		frame.getContentPane().add(texto);
+		texto.setColumns(10);
+		
+		cmb = new JComboBox();
+		cmb.setBounds(10, 61, 96, 21);
+		frame.getContentPane().add(cmb);
+		
+		btn = new JButton("Convertir");
+		btn.setBounds(116, 61, 85, 21);
+		frame.getContentPane().add(btn);
+		
+		lbl = new JLabel("00.00");
+		lbl.setBounds(116, 10, 85, 19);
+		frame.getContentPane().add(lbl);
 	}
-
 }
